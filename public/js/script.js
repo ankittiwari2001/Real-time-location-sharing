1d1a9778b58506644a7781ae48ad9a6187110430
ankittiwari2001 <at5542168@gmail.com>
2024-07-13 23:56:42 +0530

    added marker icon

--- public/js/script.js
+++ public/js/script.js
@@ -1,40 +1,88 @@
 const socket = io()
+const userForm = document.getElementById('userForm');
+const mapDiv = document.getElementById('map');
+const form = document.getElementById('form');
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position) =>{
-        const {latitude, longitude}= position.coords;
-        socket.emit("send-location", {latitude, longitude});
-    }, 
-    (error)=>{
-        console.log(error);
-    },
-    {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
+let user = {name: '', gender: ''};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    user.name = form.name.value;
+    user.gender = form.gender.value;
+    userForm.style.display = 'none';
+    mapDiv.style.display = 'block';
+    initializeMap();
+});
+
+function initializeMap() {
+    if(navigator.geolocation){
+        navigator.geolocation.watchPosition((position) =>{
+            const {latitude, longitude}= position.coords;
+            socket.emit("send-location", {latitude, longitude, ...user});
+        }, 
+        (error)=>{
+            console.log(error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        });
+    } else {
+        console.error("Geolocation is not supported by this browser.");
     }
     
-    );
+    const map = L.map("map").setView([0,0], 16)
+    
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "Ankit"
+    }).addTo(map)
+    
+    const boyIcon = L.icon({
+        iconUrl: 'icons/boy.png', 
+        iconSize: [38, 38],
+    });
+    
+    const girlIcon = L.icon({
+        iconUrl: '/icons/girl.png', 
+        iconSize: [38, 38],
+    });
+    
+    const markers = {};
+    
+    socket.on("receive-location", (data) => {
+        const {id, latitude, longitude, name, gender} = data;
+        map.setView([latitude, longitude]);
+    
+        let markerIcon = gender === 'boy' ? boyIcon : girlIcon;
+    
+        if(markers[id]){
+            markers[id].setLatLng([latitude, longitude]);
+        }
+        else{
+            markers[id] = L.marker([latitude,longitude], { icon: markerIcon})
+            .addTo(map)
+            .bindPopup(`<b>${name}</b>`)
+            .on('click', () => {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition((position) => {
+                        const user_lat_lng = [position.coords.latitude, position.coords.longitude];
+                        const destination_lat_lng = [latitude, longitude];
+                        L.Routing.control({
+                            waypoints: [
+                                L.latLng(user_lat_lng),
+                                L.latLng(destination_lat_lng)
+                            ],
+                            routeWhileDragging: true
+                        }).addTo(map);
+                    });
+                }
+            });
+        }
+    });
 }
 
-const map = L.map("map").setView([0,0], 16)
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "Ankit"
-}).addTo(map)
-
-const markers = {};
-
-socket.on("receive-location", (data) => {
-    const {id, latitude, longitude} = data;
-    map.setView([latitude, longitude]);
-    if(markers[id]){
-        markers[id].setLatLng([latitude, longitude]);
-    }
-    else{
-        markers[id] = L.marker([latitude,longitude]).addTo(map)
-    }
-})
 
 socket.on("user-disconnected", (id) => {
     if (markers[id]){
@@ -42,3 +90,6 @@ socket.on("user-disconnected", (id) => {
         delete markers[id];
     }
 })
+
+userForm.style.display = 'block';
+mapDiv.style.display = 'none';
